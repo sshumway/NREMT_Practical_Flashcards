ea31f06cb28686657127930e7f23a4ecbcb62289
Scott Shumway <shumwaysr@gmail.com>
2017-03-13 00:39:06 -0400

    added trauma data

--- data/data.js
+++ data/data.js
@@ -1,8 +1,9 @@
 /*
   <examName>: {
     title: "",
+    shortTitle: "",
     steps: [{
-      lineType: "(heading|default|sub|note)",
+      lineType: "(header|default|sub|note)",
       text: "",
       points: #
     }]
@@ -10,8 +11,242 @@
 */
 
 export default {
+  trauma: {
+    title: "Patient Assessment/Management - Trauma",
+    shortTitle: "Trauma",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Scene Size-up"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the scene/situation is safe"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the mechanism of injury/nature of illness"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the number of patients"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Requests additional EMS assistance if necessary"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Considers stabilization of the spine"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Primary Survey/Resuscitation"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Verbalizes general impression of the patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines responsiveness/level of consciousness"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines chief complaint/apparent life-threats"
+    }, {
+      lineType: "default",
+      points: 2,
+      text: "Airway"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Opens and assesses airway"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inserts adjunct as indicated"
+    }, {
+      lineType: "default",
+      points: 4,
+      text: "Breathing"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assess breathing"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assures adequate ventilation"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Initiates appropriate oxygen therapy"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Manages any injury which may compromise breathing/ventilation"
+    }, {
+      lineType: "default",
+      points: 4,
+      text: "Circulation"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Checks pulse"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assess skin [either skin color, temperature or condition]"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assesses for and controls major bleeding if present"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Initiates shock management [positions patient properly, conserves body heat]"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Identifies patient priority and makes treatment/transport decision (based upon calculated GCS)"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "History Taking"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Obtains baseline vital signs [must include BP, P and R]"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Attempts to obtain SAMPLE history"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Secondary Assessment"
+    }, {
+      lineType: "default",
+      points: 3,
+      text: "Head"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inspects and palpates scalp and ears"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assesses eyes"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inspects mouth, nose and assesses facial areas"
+    }, {
+      lineType: "default",
+      points: 3,
+      text: "Neck"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Checks position of trachea"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Checks jugular veins"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Palpates cervical spine"
+    }, {
+      lineType: "default",
+      points: 3,
+      text: "Chest"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inspects chest"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Palpates chest"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Auscultates chest"
+    }, {
+      lineType: "default",
+      points: 3,
+      text: "Abdoment/pelvis"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inspects and palpates abdomen"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assesses pelvis"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Verbalizes assessment of genitalia/perineum as needed"
+    }, {
+      lineType: "default",
+      points: 2,
+      text: "Lower extremities"
+    }, {
+      lineType: "sub",
+      points: 2,
+      text: "Inspects, palpates and assesses motor, sensory and distal circulatory functions"
+    }, {
+      lineType: "default",
+      points: 2,
+      text: "Upper extremities"
+    }, {
+      lineType: "sub",
+      points: 2,
+      text: "Inspects, palpates and assesses motor, sensory and distal circulatory functions"
+    }, {
+      lineType: "default",
+      points: 2,
+      text: "Posterior thorax, lumbar and buttocks"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inspects and palpates posterior thorax"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Inspects and palpates lumbar and buttocks area"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Manages secondary injuries and wounds appropriately"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Reassessment"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Demonstrates how and when to reassess the patient"
+    }]
+  },
+  medical: {},
+  bvm: {},
+  oxygen: {},
+  seatedSpine: {},
+  supineSpine: {},
+  bleeding: {},
+  cardiac: {},
+  joint: {},
   longBone: {
     title: "Long Bone Immobilization",
+    shortTitle: "Long Bone",
     steps: [{
       lineType: "default",
       text: "Takes or verbalizes appropriate PPE precautions",

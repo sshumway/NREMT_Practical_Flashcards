a78c821a109d2df543b5aabb625d33f7b2ae07ed
Scott Shumway <shumwaysr@gmail.com>
2017-03-12 22:40:01 -0400

    added data file

---
+++ data/data.js
@@ -0,0 +1,65 @@
+/*
+  <examName>: {
+    title: "",
+    steps: [{
+      lineType: "(heading|default|sub|note)",
+      text: "",
+      points: #
+    }]
+  }
+*/
+
+export default {
+  longBone: {
+    title: "Long Bone Immobilization",
+    steps: [{
+      lineType: "default",
+      text: "Takes or verbalizes appropriate PPE precautions",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Directs application of manual stabilization of the injury",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Assesses distal motor, sensory and circulatory functions in the injured extremity",
+      points: 1
+    }, {
+      lineType: "note",
+      text: "The examiner acknowledges, \"Motor, sensory and circulatory functions are present and normal.\"",
+      points: 0
+    }, {
+      lineType: "default",
+      text: "Measures the splint",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Applies the splint",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Immobilizes the joint above the injury site",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Immobilizes the joint below the injury site",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Secures the entire injured extremity",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Immobilizes the hand/foot in the position of function",
+      points: 1
+    }, {
+      lineType: "default",
+      text: "Reassesses distal motor, sensory and circulatory functions in the injured extremity",
+      points: 1
+    }, {
+      lineType: "note",
+      text: "The examiner acknowledges, \"Motor, sensory and circulatory functions are present and normal.\"",
+      points: 0
+    }]
+  }
+};

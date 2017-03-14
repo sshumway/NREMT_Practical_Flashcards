da0171af46c1dcdd9ea9e4cd882a504b30e17a0a
Scott Shumway <shumwaysr@gmail.com>
2017-03-13 21:20:45 -0400

    added all the exams

--- app/data/data.js
+++ app/data/data.js
@@ -49,11 +49,11 @@ export default {
     }, {
       lineType: "default",
       points: 1,
-      text: "Verbalizes general impression of the patient"
+      text: "Verbalizes the general impression of the patient"
     }, {
       lineType: "default",
       points: 1,
-      text: "Determines responsiveness/level of consciousness"
+      text: "Determines responsiveness/level of consciousness (AVPU)"
     }, {
       lineType: "default",
       points: 1,
@@ -236,14 +236,666 @@ export default {
       text: "Demonstrates how and when to reassess the patient"
     }]
   },
-  medical: {},
-  bvm: {},
-  oxygen: {},
-  seatedSpine: {},
-  supineSpine: {},
-  bleeding: {},
-  cardiac: {},
-  joint: {},
+  medical: {
+    title: "Patient Assessment/Management - Medical",
+    shortTitle: "Medical",
+    steps: [{
+      lineType: "default",
+      point: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "header",
+      point: 0,
+      text: "Scene Size-Up"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the scene/situation is safe"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the mechanism of injury/nature of illness"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the number of patients"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Requests additional EMS assistance if necessary"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Considers stabilization of the spine"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Primary Survey/Resuscitation"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Verbalizes the general impression of the patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines responsiveness/level of consciousness (AVPU)"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines chief complaint/apparent life-threats"
+    }, {
+      lineType: "default",
+      points: 3,
+      text: "Assesses airway and breathing"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assessment"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assures adequate ventilation"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Initiates appropriate oxygen therapy"
+    }, {
+      lineType: "default",
+      points: 3,
+      text: "Assesses circulation"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assesses/controls major bleeding"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Checks pulse"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Assesses skin [either color, temperature or condition]"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Identifies patient priority and makes treatment/transport decision"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "History Taking"
+    }, {
+      lineType: "default",
+      points: 8,
+      text: "History of the present illness"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Onset"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Provocation"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Quality"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Radiation"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Severity"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Time"
+    }, {
+      lineType: "sub",
+      points: 2,
+      text: "Clarifying questions of associated signs and symptoms related to OPQRST"
+    }, {
+      lineType: "default",
+      points: 5,
+      text: "Past medical history"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Allergies"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Medications"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Past pertinent history"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Last oral intake"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Events leading to present illness"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Initiates appropriate oxygen therapy"
+    }, {
+      lineType: "default",
+      points: 5,
+      text: "Assesses affected body parts/system"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Cardiovascular"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Pulmonary"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Neurological"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Musculoskeletal"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Integumentary"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "GI/GU"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Reproductive"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Psychological/Social"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Vital Signs"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Blood pressure"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Pulse"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Respiratory rate"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Respiratory quality"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "States field impression of patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Interventions [verbalizes proper interventions/treatment]"
+    }, {
+      lineType: "header",
+      points: 0,
+      text: "Reassessment"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Demonstrates how and when to reassess the patient to determine changes in condition"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Provides accurate verbal report to arriving EMS unit"
+    }]
+  },
+  bvm: {
+    title: "BVM Ventilation Of An Apneic Adult Patient",
+    shortTitle: "BVM",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Checks responsiveness"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Requests additional EMS assistance"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Checks breathing and pulse simultaneously"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "After checking responsiveness, then checking breathing and pulse for no more than 10 seconds, examiner informs candidate, \"The patient is unresponsive, apneic and has a weak pulse of 60.\""
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Opens airway properly"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now inform the candidate, \"The mouth is full of secretions and vomitus.\""
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Prepares rigid suction catheter"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Turns on power to suction device or retrieves manual suction device"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Inserts rigid suction catheter without applying suction"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Suctions mouth and oropharynx"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now inform the candidate, \"The mouth and oropharynx are clear.\""
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Opens the airway manually"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Inserts oropharyngeal airway"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now inform the candidate, \"No gag reflex is present and the patient accepts the airway adjunct.\""
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Ventilates the patient immediately using a BVM device unattached to oxygen"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now inform the candidate that ventilation is being properly performed without difficulty"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Re-checks pulse for no more than 10 seconds"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Attaches the BVM assembly [mask, bag, reservoir] to oxygen [15 L/minute]"
+    }, {
+      lineType: "default",
+      points: 2,
+      text: "Ventilates the patient adequately"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Proper volume to cause visible chest rise"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Proper rate [10 - 12/minute (1 ventilation every 5 - 6 seconds)]"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now ask the candidate, \"How would you know if you are delivering appropriate volumes with each ventilation?\""
+    }]
+  },
+  oxygen: {
+    title: "Oxygen Administration By Non-rebreather Mask",
+    shortTitle: "O2 Administration",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Gathers appropriate equipment"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Cracks valve on the oxygen tank"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Assembles regulator on the oxygen tank"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Opens the oxygen tank valve"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Checks oxygen tank pressure"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Checks for leaks"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Attaches non-rebreather mask to correct port of regulator"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Turns on oxygen flow to prefill reservoir bag"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Adjust regulator to assure oxygen flow rate of at least 10 L/minute"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Attaches mask to patient's face and adjusts to fit snugly"
+    }]
+  },
+  seatedSpine: {
+    title: "Spinal Immobilization (Seated Patient)",
+    shortTitle: "Seated Spinal Immobilization",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Directs assistant to place/maintain head in the neutral, in-line position"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Directs assistant to maintain manual stabilization of the head"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Reassesses motor, sensory and circulatory functions in each extremity"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Applies appropriately sized extrication collar"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Positions the immobilization device behind the patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Secures the device to the patient's torso"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Evaluates torso fixation and adjust as necessary"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Evaluates and pads behind the patient's head as necessary"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Secures the patient's head to the device"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Verbalizes moving the patient to a long backboard"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Reassesses motor, sensory and circulatory function in each extremity"
+    }]
+  },
+  supineSpine: {
+    title: "Spinal Immobilization (Supine Patient)",
+    shortTitle: "Supine Spinal Immobilization",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Directs assistant to place/maintain head in the neutral, in-line position"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Directs assistant to maintain manual stabilization of the head"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Reassesses motor, sensory and circulatory functions in each extremity"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Applies appropriately sized extrication collar"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Positions the immobilization device appropriately"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Directs movement of the patient onto the device without compromising the integrity of the spine"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Applies padding to the void between the torso and the device as necessary"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immobilizes the patient's torso to the device"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Evaluates and pads behind the patient's head as necessary"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immobilizes the patient's head to the device"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Secures the patient's legs to the device"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Secures the patient's arms to the device"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Reassesses motor, sensory and circulatory function in each extremity"
+    }]
+  },
+  bleeding: {
+    title: "Bleeding Control/Shock Management",
+    shortTitle: "Bleeding/Shock",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Applies direct pressure to the wound"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now inform candidate that the wound continues to bleed"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Applies tourniquet"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner must now inform candidate that the patient is exhibiting signs and symptoms of hypoperfusion"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Properly positions patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Administers high concentration oxygen"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Initiates steps to prevent heat loss from the patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Indicates need for immediate transportation"
+    }]
+  },
+  cardiac: {
+    title: "Cardiac Arrest Management / AED",
+    shortTitle: "Cardiac/AED",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Determines the scene/situation is safe"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Attempts to question bystanders about arrest event"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Checks patient responsiveness"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Requests additional EMS assistance"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Checks breathing and pulse simultaneously"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "After checking responsiveness, then checking breathing and pulse for no more than 10 seconds, examiner informs candidate, \"The patient is unresponsive, apneic and pulseless.\""
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immediately begins chest compressions [adequate depth and rate; allows the chest to recoil completely]"
+    }, {
+      lineType: "default",
+      points: 5,
+      text: "Performs 2 minutes of high-quality, 1-rescuer adult CPR"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Adequate depth and rate"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Correct compression-to-ventilation ratio"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Allows the chest to recoil completely"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Adequate volumes for each breath"
+    }, {
+      lineType: "sub",
+      points: 1,
+      text: "Minimal interruptions of no more than 10 seconds throughout"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "After 2 minutes (5 cycles), candidate assesses patient and second rescuer resumes compressions while candidate operates AED"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Turns on power to AED"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Follows prompts and correctly attaches AED to patient"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Stops CPR and ensures all individuals are clear of the patient during  analysis"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Ensures that all individuals are clear of the patient and delivers shock from AED"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immediately directs rescuer to resume chest compressions"
+    }]
+  },
+  joint: {
+    title: "Joint Immobilization",
+    shortTitle: "Joint Immobilization",
+    steps: [{
+      lineType: "default",
+      points: 1,
+      text: "Takes or verbalizes appropriate PPE precautions"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Directs application of manual stabilization of the injury"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Assesses distal motor, sensory and circulatory functions in the injured extremity"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner acknowledges, \"Motor, sensory and circulatory functions are present and normal.\""
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Selects the proper splinting material"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immobilizes the site of the injury"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immobilizes the bone above the injury site"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Immobilizes the bone below the injury site"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Secures the entire injured extremity"
+    }, {
+      lineType: "default",
+      points: 1,
+      text: "Reassesses distal motor, sensory and circulatory functions in the injured extremity"
+    }, {
+      lineType: "note",
+      points: 0,
+      text: "The examiner acknowledges, \"Motor, sensory and circulatory functions are present and normal.\""
+    }]
+  },
   longBone: {
     title: "Long Bone Immobilization",
     shortTitle: "Long Bone",

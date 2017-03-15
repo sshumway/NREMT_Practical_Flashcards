bf087bf6223ad415347a27722ffad721fa4123e8
Scott Shumway <shumwaysr@gmail.com>
2017-03-15 17:40:43 -0400

    removed unecessary stuff

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+
 import Navigator from './app/views/Navigator/NavigatorView';
 
 export default class NREMT_Practical_Flashcards extends Component {
@@ -20,24 +10,5 @@ export default class NREMT_Practical_Flashcards extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('NREMT_Practical_Flashcards', () => NREMT_Practical_Flashcards);
